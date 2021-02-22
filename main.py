d82764a90245e6fb7ed32df9830be06f1fdb313e
gf4678 <s1771079@s.konan-u.ac.jp>
2021-02-22 19:02:39 +0900

    1行ごとに読み込むように変更

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import glob
 from janome.tokenizer import Tokenizer
+from operator import itemgetter
+# from MeCab import Tagger
 
 t = Tokenizer()
+# mecab = Tagger("-Ochasen")
 def importLyric(dirname):
     dirname += "/*.txt"
     lyrics = []
@@ -9,24 +12,36 @@ def importLyric(dirname):
     # # print(lyric_file, flush=True)
 
     for j in lyric_file:
-        with open(j, mode='r') as f:
+        with open(j, mode='r', encoding="utf-8") as f:
             lyrics.append(f.read())
     return lyrics
 
 
 def analysis(lyrics):
     word = {}
-    for i in lyrics:
-        wakati = list(t.tokenize(i, wakati=True))
+    for lyric in lyrics:
+        # print(i)
+        lines = lyric.split("\n")
+        wakati = []
+        for line in lines:
+            wakati.extend(list(t.tokenize(line, wakati=True)))
+        # node = mecab.parseToNode(i)
         for j in range(len(wakati)):
             if wakati[j] not in word:
-                word[wakati[j]] = 1
-            elif j == 0 or wakati[j] not in wakati[:j-1]:
+                word[wakati[j]] = 0
+            if j == 0 or wakati[j] not in wakati[:j-1]:
                 word[wakati[j]] += 1
     return word
 
 
-print(analysis(importLyric("Lyrics")))
+word = analysis(importLyric("amazarashi"))
+word = sorted(word.items(), key=itemgetter(1))
+word.reverse()
+word = word[:100]
+for i in word:
+    # print(i)
+    # print(word[i])
+    print(str(i[0])+"："+str(i[1]))
 
 
 
